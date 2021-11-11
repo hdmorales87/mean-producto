3e9d17426bc4f3b13cb541b54db6e5f19ae03711
hector morales <hector.morales@logicalsoft.co>
2021-11-11 16:53:12 -0500

    11112021 primer commit

---
+++ servidor/controllers/productoController.js
@@ -0,0 +1,88 @@
+const Producto = require("../models/Producto");
+
+
+exports.crearProducto = async (req,res) => {
+    try{
+        let producto;
+
+        //creamos nuestro producto
+        producto = new Producto(req.body);
+
+        await producto.save();
+        return res.send(producto);
+
+    } catch(error){
+        console.log(error);
+        return res.status(500).send('Hubo un error');
+    }
+}
+
+exports.obtenerProductos = async (req,res) => {
+    try{
+        
+        const productos = await Producto.find();
+        return res.json(productos);
+
+    } catch(error){
+        console.log(error);
+        return res.status(500).send('Hubo un error');
+    }
+}
+
+exports.actualizarProducto = async (req,res) => {
+    try{
+        
+        const { nombre, categoria, ubicacion, precio } = req.body;
+        let producto = await Producto.findById(req.params.id);
+
+        if(!producto){
+            return res.status(404).json({ msg: 'No existe el producto' });
+        }
+
+        producto.nombre = nombre;
+        producto.categoria = categoria;
+        producto.ubicacion = ubicacion;
+        producto.precio = precio;
+
+        producto = await Producto.findOneAndUpdate({ _id: req.params.id }, producto, { new: true });
+        return res.json(producto);
+
+    } catch(error){
+        console.log(error);
+        return res.status(500).send('Hubo un error');
+    }
+}
+
+exports.obtenerProducto = async (req,res) => {
+    try{        
+        let producto = await Producto.findById(req.params.id);
+
+        if(!producto){
+            return res.status(404).json({ msg: 'No existe el producto' });
+        }
+
+        return res.json(producto);
+
+    } catch(error){
+        console.log(error);
+        return res.status(500).send('Hubo un error');
+    }
+}
+
+exports.eliminarProducto = async (req,res) => {
+    try{       
+        let producto = await Producto.findById(req.params.id);
+
+        if(!producto){
+            return res.status(404).json({ msg: 'No existe el producto' });
+        }
+
+        await Producto.findOneAndRemove({ _id: req.params.id });
+
+        return res.json({ msg: 'Producto eliminado con exito' });
+
+    } catch(error){
+        console.log(error);
+        return res.status(500).send('Hubo un error');
+    }
+}
